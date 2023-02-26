c700a22df3acc835e643259da74a7b53c138178d
LeviMSmith <levimsmith089@gmail.com>
2023-02-25 19:00:28 -0700

    More color changes

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -3,11 +3,11 @@ if status is-interactive
 
     #Colors
     set -l color_secondary E4F0FB
-    set -l color_error D0679D
 
     set -g fish_color_normal $color_secondary
     set -g fish_color_user $color_secondary
     set -g fish_color_cwd $color_secondary
 
-    set -g fish_color_error $color_error
+    set -g fish_color_error D0679D
+    set -g fish_color_quote 767C9D
 end
